6398fb6d79fc0a1e619e7e1dc1e22c6566915f20
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-04-29 10:19:45 -0300

    taoyao: Adding taoyao blobs from V816.0.12.0.ULIMIXM

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),taoyao)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,d75b1a77e2788ed24f28609fa6a91ab288a378c9)
+$(call add-radio-file-sha1-checked,radio/aop.img,4f6b7391f24c9f6abc4959cb668f74a12255cdd3)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,56d4a300252f273fce218a40bbfe024f431c21b2)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,ba4d7830f2945e7b6431b7c3c8c72c7877cd9b78)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,824a2359c01f41cae9bcecac3611dbd7ba19652f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,d3fdbfa43b8686991d86c6679c3a87b07a11b368)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,752de6331264ce7242a5f0ccb5af9cd0fb10a667)
+$(call add-radio-file-sha1-checked,radio/hyp.img,4b2dd74bead117aec9c49389a33e71d988140fe6)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,8ec9e88657e5ccc23d945897ac0595f00f83d98c)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,2145865f50c783b91fabd4d542a34439f3974dfe)
+$(call add-radio-file-sha1-checked,radio/modem.img,1b5797c2fa6de227667e4d6f3d363c642fce3a10)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,59346317e0f2b43929254a10364c0986cdff899d)
+$(call add-radio-file-sha1-checked,radio/shrm.img,74f2abc7964f01b2c13e4d81dbecd1a30cc3dd55)
+$(call add-radio-file-sha1-checked,radio/tz.img,5c7a4e58e01add17e40c1edb7eb36e2821651cc6)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,ec9a75cde165eb6015838c93160d15c091c0aead)
+$(call add-radio-file-sha1-checked,radio/xbl.img,62d9468d1f9125535faf59d00c09eaa3536465a8)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,ee82102340729a2683db7af63052091a03dde71f)
+
+endif
